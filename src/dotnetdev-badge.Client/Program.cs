85c4e1c3a183cc713e09db8e5064a4dab5cfb24f
chanos-dev <chanos.dev@gmail.com>
2024-01-04 22:58:49 +0900

    ✨ feat - copy to clipboard 추가
    🎈 update
    - favicon 변경
    - loading 문구 수정

--- src/dotnetdev-badge.Client/Program.cs
+++ src/dotnetdev-badge.Client/Program.cs
@@ -8,6 +8,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices();
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
