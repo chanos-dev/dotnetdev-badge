d40ab5ac764ad052d2964a1d47b5510d1b39979d
chanos-dev <chanos.dev@gmail.com>
2024-01-04 23:09:25 +0900

    blazor hosting

--- src/dotnetdev-badge.web/Interfaces/IMeasureText.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DotNetDevBadgeWeb.Interfaces
-{
-    interface IMeasureText
-    {
-        bool IsMediumIdWidthGreater(string id, out float addedWidth);
-    }
-
-    interface IMeasureTextV1 : IMeasureText { }
-}
